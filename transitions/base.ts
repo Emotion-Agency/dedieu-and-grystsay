4919a9971b225219e87a686a84f370e5c4cc63e8
letmewin22 <lkosteckiy5@gmail.com>
2025-08-04 09:58:22 +0300

    fix: transition

--- transitions/base.ts
+++ transitions/base.ts
@@ -2,19 +2,12 @@ import type { TransitionProps } from 'vue'
 import { gsap } from '../libs/gsap'
 import { basicObject } from './basicObject'
 
-const duration = 2
+const duration = 0.5
 const ease = 'power3.inOut'
 
 export const pageTransition: TransitionProps = {
   mode: 'out-in',
   onEnter(el, done) {
-    const revealer = document.querySelector('.revealer') as HTMLElement
-
-    if (!revealer) {
-      done()
-      return
-    }
-
     const tl = gsap.timeline({
       defaults: {
         duration,
@@ -25,39 +18,23 @@ export const pageTransition: TransitionProps = {
 
         tl.revert()
         tl.kill()
-
-        revealer.style.display = 'none'
       },
-    })
-
-    tl.to(revealer, {
-      clipPath: 'inset(0 0 100% 0)',
     })
 
     tl.fromTo(
       el,
       {
-        y: 100,
         opacity: 0,
       },
       {
-        y: 0,
         opacity: 1,
       },
-      0
+      0.3
     )
 
     basicObject.onEnter()
   },
-
   async onLeave(el, done) {
-    const revealer = document.querySelector('.revealer') as HTMLElement
-
-    if (!revealer) {
-      done()
-      return
-    }
-
     const tl = gsap.timeline({
       onComplete: done,
       defaults: {
@@ -66,29 +43,13 @@ export const pageTransition: TransitionProps = {
       },
     })
 
-    tl.set(revealer, {
-      clipPath: 'inset(100% 0 0 0)',
-      display: 'block',
-      opacity: 1,
-    })
-
-    tl.to(
-      revealer,
-      {
-        clipPath: 'inset(0% 0 0 0)',
-      },
-      0.1
-    )
-
     tl.to(
       el,
       {
-        y: -200,
         opacity: 0,
       },
       0
     )
-
     basicObject.onLeave()
   },
 }
